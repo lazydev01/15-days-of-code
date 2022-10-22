abcf2f1d0fca551fcee60f4cbac71469d559f854
lazydev01 <98304349+lazydev01@users.noreply.github.com>
2022-10-22 16:18:57 +0530

    Create Equality_in_array.cpp

---
+++ Equality_in_array.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    int pow = log2(n);
+    for(int i=0; i<pow; i++){
+        vector<int> temp;
+        for(int j=0; j<n/2; j++){
+            if(j%2==0){
+                temp.push_back(min(arr[2*j], arr[(2*j)+1]));
+            }
+            else{
+                temp.push_back(max(arr[2*j], arr[(2*j)+1]));
+            }
+        }
+        // for(int k=0; k<temp.size(); k++){
+        //     cout << temp[k] << " ";
+        // }
+        // cout << endl;
+        n/=2;
+        arr = temp;
+    }
+    cout << arr[0] << endl;
+return 0;
+}
